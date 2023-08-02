d772a78b325a298e3603654ce9171d88b7932888
vemaroopesh <40521256+vemaroopesh@users.noreply.github.com>
2023-08-02 19:49:09 +0530

    Update streamlit_app.py
    updated header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 streamlit.title('My Parents new Healthy Dinner')
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 streamlit.text(' 🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🐔Hard Boiled Free-Range Egg')
-streamlit.text('🥑Avocado Toast ')
+streamlit.text('🥑Avocado Toast')
